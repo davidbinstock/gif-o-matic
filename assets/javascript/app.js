a78d7f6a4702372009da96971118873e3e3822ac
davidbinstock <binstock.d@gmail.com>
2018-03-23 18:54:27 -0500

    now multiple gifs show up with ratings

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,7 +17,7 @@ $(document).on("click", ".topic-button", function(){
     console.log("you clicked on " + $(this).attr("data-topic"))
     var currentTopic = $(this).attr("data-topic");
     //make query string
-    var queryString = "https://api.giphy.com/v1/gifs/random?api_key="+apiKey+"&tag=" + currentTopic ;
+    var queryString = "https://api.giphy.com/v1/gifs/search?limit=10&api_key="+apiKey+"&q=" + currentTopic ;
 
     // make ajax call
     $.ajax({
@@ -25,9 +25,18 @@ $(document).on("click", ".topic-button", function(){
         method: "GET"
     }).then(function(response){
         console.log(response)
-        var imageUrl = response.data.image_original_url;
-        var newImage = $("<img>").attr("src", imageUrl).attr("alt", currentTopic );
-        $("#gif-holder").prepend(newImage)
+        var results = response.data
+        for (var i = 0; i<results.length;i++){
+            var gifDiv = $("<div>").addClass("gif-div");
+            var p = $("<p>").text("Rating: "+results[i].rating);
+            var image = $("<img>").attr("src", results[i].images.fixed_height.url);
+            gifDiv.append(p,image)
+            $("#gif-holder").prepend(gifDiv)
+
+        }
+        //var imageUrl = response.data.image_original_url;
+        //var newImage = $("<img>").attr("src", imageUrl).attr("alt", currentTopic );
+        //$("#gif-holder").prepend(newImage)
 
     })
 })
